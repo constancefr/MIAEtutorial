095da1843e944143c132c383cb72750c7dd6c398
constancefr <constance.frachon@gmail.com>
2022-01-29 11:53:27 -0500

    Utils commit

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
+from api import utils
+
 def main_wrapper():
     print(f'This is the start of our Python project. This function\'s name is {main_wrapper.__name__}')
-
     # Code here
+    utils.solid_example_1(example_param_1='a', example_param_2=1)
+    utils.solid_example_2(2.90)
+    utils.solid_example_3()
 
     print('This is the end of our Python project')
 
